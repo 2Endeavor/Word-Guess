96393d45490aa67f127cc8eb052738012cd26335
Maryanne Heinbaugh <23heinbau@gmail.com>
2019-05-30 16:38:22 -0700

    able to pull user guess off screen and place letters guessed into an array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,12 @@
+// TODO: create
+
+// TODO: Need a conditional if statement that looks at the letter guessed and determines if it is part part of the word
+//    1. if letter was previously guessed then the user should be prompted to select something else
+//    2. Is the letter part of the word's array? if yes:
+//      a. placed the letter guessed in the lettersGuessed array
+//      b. display the letter guessed on the screen
+// TODO: filter out non-letters
+
 console.log("I see you :-)");
 //Number of wins will be incremented by one with each word guessed
 var win = 20;
@@ -6,21 +15,21 @@ var win = 20;
 var capitol =["Olympia", "Nashville","Sacramento"];
 //keyWord is the capitol randomly chosen by the computer from the array capitol
 //var keyWord = capitol[2];
+// create an array of possible letters that can be guessed
+var alpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
-
-// Generating a random selection from the capitol array
+// keyWord is the word the user is trying to guess. This word is randomly generated from the capitol array.
 var keyWord = capitol[Math.floor(Math.random() * capitol.length)];
 //NOB is the number of blanks needed to represent the keyword
 var NOB = keyWord.length;
-
 var lettersGuessed = [];
-
+// The following stores the letter guessed to the lettersGuessed array
 var userText = document.getElementById("user-text");
 document.onkeyup = function(event){
   userText.textContent = event.key;
-  lettersGuessed.push(userText);
+  if(userText)
+  lettersGuessed.push(event.key);
   console.log(event.key);
-
 };
 
 
@@ -29,10 +38,7 @@ document.onkeyup = function(event){
 
 
 
-
-
-
-console.log(keyWord, NOB,lettersGuessed);
+console.log(lettersGuessed);
 
 
 
