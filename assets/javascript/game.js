cf12912d1d5ef603b402f85d40fc81fbef7df091
Maryanne Heinbaugh <23heinbau@gmail.com>
2019-05-31 14:39:36 -0700

    deleted extra files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,6 @@
 
 
-// TODO: Need a conditional if statement that looks at the letter guessed and determines if it is part part of the word
-//    1. if letter was previously guessed then the user should be prompted to select something else
-//    2. If the letter part of the word's array:
-//      a. display the letter guessed on the screen in the proper position
-//      b. run the victory function
-//       If thte letter is not part of the word's array:
-//      a.Place the letter in the letters guessed array
-//      b. guessesRemaining--
-//      c. run the victory function
+
 //     
 //      
 // TODO: filter out non-letters
@@ -32,26 +24,41 @@ var win = 0;
 
 // capitol is the array 50 capitols that are possible words
 var capitol =["Olympia", "Nashville","Sacramento"];
-//keyWord is the capitol randomly chosen by the computer from the array capitol
-//var keyWord = capitol[2];
+
 // create an array of possible letters that can be guessed
 var alpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
-// keyWord is the word the user is trying to guess. This word is randomly generated from the capitol array.
+// keyWord is the word the user is trying to guess. This word is randomly selected from the capitol array.
 var keyWord = capitol[Math.floor(Math.random() * capitol.length)];
-//NOB is the number of blanks needed to represent the keyword
+
+//NOB is the Number Of Blanks needed to represent the keyword
 var NOB = keyWord.length;
+
 var lettersGuessed = [];
-// The following stores the letter guessed to the lettersGuessed array
+// The following stores the letter selected to a temporary variable
 var userText = document.getElementById('user-text');
 document.onkeyup = function(event){
   userText.textContent = event.key;
-  event.
+  //Now we store the letter to the letterGuessed array. Pushes the variable event.key to the end of the letterGuessed array
   if(userText)
   lettersGuessed.push(event.key);
   console.log(event.key);
+  console.log(letterGuessed);
 };
 
+
+// TODO: Need a conditional if statement that looks at the letter guessed (var: letterGuessed) and determines if it is part part of the ///// word (var: [keyWord])
+//    1. Make sure the key pressed is a letter. Compare the letter pressed (var: letterGuessed) to the array of possible selections (var: [alpha] if result is a -1 tell user to select a letter)
+//console.log(letterGuessed);
+
+//    2. if letter was previously guessed then the user should be prompted to select something else
+//    3. If the letter part of the word's array:
+//      a. display the letter guessed on the screen in the proper position
+//      b. run the victory function
+//       If the letter is not part of the word's array:
+//      a.Place the letter in the letters guessed array
+//      b. guessesRemaining--
+//      c. run the victory function
 
 
 
