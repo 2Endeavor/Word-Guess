82dc45f1518595b5610f85c2493e0c0d06c8941a
Maryanne Heinbaugh <23heinbau@gmail.com>
2019-06-01 19:42:33 -0700

    got game to work!

--- assets/javascript/--game.js
+++ assets/javascript/--game.js
@@ -1,7 +1,7 @@
 // GLOBAL VARIABLES
 var win = 0; // tracks wins
 var wordList = ["Olympia", "Nashville", "Sacramento"]; //list of words computer selects from
-
+var fullAlphabetList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; // letters allowed
 
 var activeWordForGame = "";
 var activeWordLength = 0;    // This is the length of the random word
@@ -19,7 +19,6 @@ var currentGuessDiv = document.getElementById('user-text');
 
 // start game function
 function startGame(parameter1) {
-  console.log('the game is starting');
   // things i need to do to start a new game
   guessesRemaining = 10;
   activeWordForGame = wordList[Math.floor(Math.random() * wordList.length)]; // select a word
@@ -35,60 +34,57 @@ function startGame(parameter1) {
 
   // throw all the dashes into a string and then put a space between each dash
   document.getElementById("dashes").innerHTML = blanksAndSuccesses.join(" ");
-  // checkLetter(activeWordLength, activeWordForGame, currentGuessDiv) // but there is no letter right now
+  checkLetter(activeWordLength, activeWordForGame, currentGuessDiv)
  
   // for every letter that has been guessed let's show that to the user with a space separating
   document.getElementById("guesses").innerHTML = lettersGuessed.join(" ");
   document.getElementById("remainingGuesses").innerHTML = guessesRemaining;
-  // end draw board
 } 
-
-function drawBoard() {
-  document.getElementById("dashes").innerHTML = blanksAndSuccesses.join(" ");
-  document.getElementById("guesses").innerHTML = lettersGuessed.join(" ");
-  document.getElementById("remainingGuesses").innerHTML = guessesRemaining;
-}
 
   // *****check letters function******
   // for loop to loop thru the array to compare keyword with user arrays; use if else statement to compare
   // if it is there, add letter to guessesandSuccesses array . if it doesn't exist (else statement) push the letter into the letters guessed array and subtract 1 from guesses remaining
-function checkLetter(letterGuessed) {
-  for (i = 0; i < activeWordLength; i++) { // look at ever letter in word
-    var currentLetter = activeWordForGame[i].toUpperCase(); // assign the current letter to a variable
-    // is the current letter in the word equal to the letter i guessed
-    if(currentLetter === letterGuessed.toUpperCase()){
-      blanksAndSuccesses[i] = currentLetter;
-    }
-  }
-}
+function checkLetter() {
+  for (i = 0; i < activeWordLength; i++) {
+    LetterinWord= activeWordForGame[i]
+    console.log(currentGuessDiv);
+    
+    
+    // if (event.key.indexOf) {
+    //   console.log(event.key.indexOf);
+      
+      // console.log(keyWord, "this is not a letter in the word")
+
+  //   } else { "this letter is in position" + i };
+  // }
+  } // End checkletter loop
+} // end check letter
 
 
 // *****Round complete function******
 // else if statement to see if the user won or loss 
 // start game function will be called from here
 
-function processRound(letterGuessed) {
-  checkLetter(letterGuessed); // find out if it's a match
-  drawBoard();
-  // check if the game is over
 
-
-  //Now we store the letter to the letterGuessed array. Pushes the variable event.key to the end of the letterGuessed array
-  if (currentGuessDiv) {
-    checkLetter(event.key);
-    lettersGuessed.push(event.key.toUpperCase());
-  }
-  //change the mystery word from a string into an array
-  lettersThatHaveBeenGuessed = activeWordForGame.split(""); //converts the 
-  //look at each letter guessed and determine if it is in the W array
-}
-
-document.onkeyup = function (event) {
-  currentGuessDiv.textContent = event.key;
-  processRound(event.key);
-  event.key = " "
-};
 
 
 //MAIN PROCESS
 startGame(currentGuessDiv);
+document.onkeyup = function (event) {
+  currentGuessDiv.textContent = event.key;
+
+ 
+  //Now we store the letter to the letterGuessed array. Pushes the variable event.key to the end of the letterGuessed array
+  if (currentGuessDiv)
+    lettersGuessed.push(event.key);
+  //change the mystery word from a string into an array
+  lettersThatHaveBeenGuessed = activeWordForGame.split(""); //converts the 
+  //  console.log (W[0]);
+  //look at each letter guessed and determine if it is in the W array
+
+
+
+
+  event.key = " "
+  
+};
