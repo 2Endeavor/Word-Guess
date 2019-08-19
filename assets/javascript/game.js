7f60add3cde450d089a7e2c568e11a5ec200452f
Maryanne Heinbaugh <23heinbau@gmail.com>
2019-08-19 14:15:29 -0700

    resolving edge cases

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-// TODO: fix edge cases. Player should only be able to select alph characters. Players shouldn't lose additional guesses when the enter the same wrong letter more than once 
+// TODO: fix edge cases. Player should only be able to select alpha characters. Players shouldn't lose additional guesses when the enter the same wrong letter more than once 
 // TODO: add hints when a letter is guessed. Each state should include the general geographical location, The year it became a state, the states  top products, The states number it joined the union
 // TODO: encouraging words when they have three letters remaining "You're so close, you've almost got it!"
 // TODO: when the answer is revieled I want to show a picture of the state and capitol. Maybe a photo and maybe with the continental US and a pin to show where the state and capitol is on the map
@@ -16,7 +16,7 @@ var mysteryWord = []; // array of letters for word to be guessed
 var lettersGuessed = []; // array of letters guessed
 var blanksAndSuccesses = []; // this will hold the blanks and correct guesses
 var currentGuessDiv = document.getElementById('user-text');// The following stores the letter selected to a temporary variable
-var match = false;
+
 
 
 
@@ -24,6 +24,7 @@ var match = false;
 // start game function runs and gets the game ready. Once this function runs nothing happens until the player pushes a button, activating function(event)
 function startGame(parameter1) {
   console.log('the game is starting line 15');
+  guessesRemaining = 10; // tracks the number of guesses remaining
  
   
   activeWordForGame = wordList[Math.floor(Math.random() * wordList.length)]; // select a word
@@ -62,7 +63,7 @@ function drawBoard() {
 // *****check letters function******
 //Loop thru the activeWordForGame array and check to see if letter Guessed is a match for any element
 function checkLetter(letterGuessed) {
-  match = false;
+ 
 // look at every letter in the word
   console.log("line 56 letterGuessed " + letterGuessed)
   for (i = 0; i < activeWordLength; i++) { 
@@ -76,8 +77,7 @@ function checkLetter(letterGuessed) {
 
         console.log("66 current letter line " + currentLetter)
         console.log("67 letterGuessed  line " + letterGuessed)
-        console.log("68 the letter matched! ")
-        match = true;  
+
     
     };
   }
@@ -85,15 +85,32 @@ function checkLetter(letterGuessed) {
 
 };
 
+// function to look for edge cases
+// TODO: letter has already been guessed
+// TODO: character entered is not a letter
+function edgeCases(character){
+  let charCode = character.charCodeAt(0);
 
+  if ((charCode>64 && charCode<91) ||(charCode>96 && charCode<123)) {
+    console.log("this is a letter")
+    
+  }
+  else{
+    console.log("this is not a letter")
+    document.getElementById('user-text').innerHTML ="Not a Valid Entry";
+   
+
+  }
+}
 
 
 
 
 // processRound function looks at the letter selected checks to see if the letter guessed is a match to a letter in the word
 function processRound(letterGuessed) {
+  edgeCases(letterGuessed);
   console.log("line 83 Here I am")
-  checkLetter(letterGuessed); // find out if it's a match
+  checkLetter(letterGuessed); // find out if it's a 
   console.log("line 85 letterGuessed " +letterGuessed)
   drawBoard();
   console.log("line 87 letterGuessed " +letterGuessed)
@@ -108,37 +125,37 @@ function processRound(letterGuessed) {
 
 };
 
-//functions gameStatus() is involked from the event function. It takes in the letter guessed and if it was a match evaluate game 
+//gameStatus() function is involked from the event function. It takes in the letter guessed and the mystery word
 function gameStatus(letter, word){
-  // evaluate if the letter guessed was wrong
+  // If letter guessed is wrong, decrement guessesRemaining and update board
   if((mysteryWord.indexOf(letterToEvaluate))===-1){
-    //decrement the guessesRemaining by -1
     guessesRemaining --
-    //update the board with the number of guesses remaining
     document.getElementById("remainingGuesses").innerHTML = guessesRemaining;
   };
-    // TODO: If guessesRemaining = 0 notify the player of the correct word and stop game
-    if(guessesRemaining ===0){
-      document.getElementById('user-text').innerHTML ="The Capitol is: " + activeWordForGame;
-      console.log("you have no guesses remaining")
-      console.log("the correct word is" + mysteryWord);
 
-    }
+  //If guessesRemaining = 0 notify the player of the correct word 
+  if(guessesRemaining ===0){
+      document.getElementById('user-text').innerHTML ="The Capitol is: " + activeWordForGame;
+      console.log("line 122 gameover player ran out of guesses")
+      startGame();
+  }
+
+  // Check to see if all letters have been guessed
+  if (blanksAndSuccesses.indexOf("_")===-1){
+      document.getElementById('user-text').innerHTML ="That's Correct. Well Done!"; 
+      // TODO: increment the number of wins by +1
+      console.log("line 128 startgame function called on line 128")
+      startGame();    
+  };
 
 }
 
-// TODO:  if the letter has been correctly guessed
-  // TODO: have all the letters been guessed?
-      //TODO: if yes, 
-          // TODO: increment the number of wins by +1
-          //TODO: notifify player and stop the game
-
-      //TODO: if no, allow the player to play again
 
 
 
 // When a key is pressed, this function calls processRound and passes the key pressed
 document.onkeyup = function (event) {
+
   console.log("Line 102 Here I am")
   currentGuessDiv.textContent = event.key.toUpperCase();
   processRound(event.key);
@@ -147,7 +164,7 @@ document.onkeyup = function (event) {
   gameStatus(letterToEvaluate, mysteryWord)
   console.log("letterToEvaluate " + letterToEvaluate)
   console.log("line 106 event.key " + event.key)
-  console.log("match " + match);
+
   console.log("mysteryWord " + mysteryWord);
   console.log("letterGuessed " + lettersGuessed)
   console.log(mysteryWord.indexOf(letterToEvaluate))
